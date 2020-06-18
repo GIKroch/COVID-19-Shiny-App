eebeb01bef610146e80efcb7a8a7579eede1551c
kasiakry <32612075+kasiakry@users.noreply.github.com>
2020-06-18 18:47:55 +0200

    Update server.R
    Change to /tmp folder

--- server.R
+++ server.R
@@ -220,7 +220,8 @@ shinyServer(function(input, output, session) {
       #                  params = params,
       #                  envir = new.env(parent = globalenv())
       
-      report_path <- tempfile(fileext = ".Rmd",tmpdir="/tmp/R")
+      report_path <- tempfile(fileext = ".Rmd",tmpdir="/tmp")
+      file.copy("markdown/markdown.css", "/tmp/markdown.css")
       file.copy("markdown/output.Rmd", report_path, overwrite = TRUE)
       
       rmarkdown::render(report_path, 
@@ -254,7 +255,8 @@ shinyServer(function(input, output, session) {
       #                  params = params,
       #                  envir = new.env(parent = globalenv())
       
-      report_path <- tempfile(fileext = ".Rmd",tmpdir="/tmp/R")
+      report_path <- tempfile(fileext = ".Rmd",tmpdir="/tmp")
+      file.copy("markdown/markdown.css", "/tmp/markdown.css")
       file.copy("markdown/output.Rmd", report_path, overwrite = TRUE)
       
       rmarkdown::render(report_path, 
